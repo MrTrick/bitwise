abcce22e7e29b148e56923ac92df2b77e50ef135
MrTrick <MrTrick@github>
2016-01-08 17:01:34 +1100

    Cleanup

--- latin.c
+++ latin.c
@@ -2,14 +2,12 @@
 #include "stdint.h"
 #include "stdio.h"
 #include "string.h"
+#include "unistd.h"
 
 #define ELEM uint16_t
 #define ffs __builtin_ffs
 
-//      print_bits(free[j],N); putchar(' ');
-//      if (!((j+1)%N)) putchar('|');
-//   }
-//}
+//Exhaustively calculate *all* possible latin squares of size N, sending solutions back to the given function.
 int latin(ELEM sq[], const uint8_t N, void cb(ELEM[],const uint8_t)) {
    const ELEM ALLb=(1<<N)-1;
 
@@ -26,28 +24,19 @@ int latin(ELEM sq[], const uint8_t N, void cb(ELEM[],const uint8_t)) {
    free[i]=hfree[i]&vfree[i];
 
    loop_el: do {                                   // While there is search space
-      //printf("loop_el - i:%d, free[i]:",i);print_bits(free[i],N);printf("\n");
       while ((el=free[i]&-free[i])) {              // While there are valid and unchecked values at [i]
                                                    //     (el = lowest '1', eg if free[i] is 0b101100, el is 0b000100)
          free[i]&=~el;                             // Mark el as checked
          if ((i+1)%N) hfree[i+1]=hfree[i]&~el;     // Value can't be used to the right
          if ((i+N)<N*N) vfree[i+N]=vfree[i]&~el;   // Value can't be used underneath
-         sq[i]=el;                                 // Record in table
-
-         //printf("free[i] is valid;  el:");print_bits(el,N);printf("\n");
-         //printf("  hfree:"); print_free(hfree,N,i); printf("\n");
-         //printf("  vfree:"); print_free(vfree,N,i); printf("\n");
-         //printf("   free:"); print_free(free,N,i); printf("\n");
-         //printf(" square:"); print_free(sq,N,i); printf("\n");
-
+         sq[i]=el;                                 // Record element
          if (i==N*N-1) {                           // Is the square complete?
-            cb(sq,N);                              // Yes; Pass back to the caller
-            count++;                               // Tally the solution
+            cb(sq,N);                              //  Yes; Pass back to the caller
+            count++;                               //       Tally the solution
          } else {
-            //printf("Pushing: "); print_bits(hfree[i],N); putchar(' '); print_bits(hfree[i+1],N); putchar('\n');
-            i++;                                   // No; PUSH!
-            free[i]=hfree[i]&vfree[i];             // Calculate available bits
-            goto loop_el;                          // Start search at next i
+            i++;                                   //  No; PUSH!
+            free[i]=hfree[i]&vfree[i];             //      Calculate available bits
+            goto loop_el;                          //      Start search at next i
          }
       }
    } while(i--);                                   // POP! No more values at this position
@@ -65,12 +54,10 @@ int main(int argc, char *argv[]) {
    uint8_t N = atoi(argv[1]);
    if (!N) { printf("ERR: Dimension must be at least one!"); exit(1); }
 
-
    ELEM sq[N*N]; memset(sq,0,sizeof(sq));    //Create state space with zeros
-
-   latin(sq,N,found_solution);
-   //printf("Found %d solutions.\n",count);
-
+   int count = latin(sq,N,found_solution);
+   if (isatty(1)) printf("Found %d solutions\n",count);
+   
    return 0;
 }
 
